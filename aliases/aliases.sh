aff928fd30a948c205eaa6fedade49ac22bd80f9
victormwangi <mkoloni550@gmail.com>
2024-04-15 16:43:33 +0300

    personal dotfiles

---
+++ aliases/aliases.sh
@@ -0,0 +1,50 @@
+alias cl="clear"
+alias clls="clear; ls"
+alias v="nvim"
+alias cp="cp -rf"
+alias rm="rm -rf"
+alias q="exit"
+alias du="du -h"
+alias halt="sudo halt"
+alias reboot="sudo reboot"
+alias top="bpytop"
+alias reminder="vi ~/Reminder/reminder.md"
+alias fm="vifm"
+
+# Cd aliases
+alias ..="cd .."
+alias ....="cd ..; cd .."
+alias ......="cd ..; cd ..; cd .."
+alias ........="cd ..; cd ..; cd ..; cd .."
+
+
+# Git aliases
+alias gi="git init"
+alias gi.="git init ."
+alias gc="git commit ."
+alias gca="git commit -a"
+alias gcm="git commit -m"
+alias ga="git add --all"
+alias ga.="git add ."
+alias gai="git add -i"
+alias gl="git log"
+alias gh="git help"
+alias gp="git push -u origin main"
+alias gs="git status"
+alias gb="git blame"
+alias grh="git reset --hard"
+
+alias ls="lsd"
+alias sl="lsd"
+
+# Python Aliases
+alias pip="pip"
+alias py="python3"
+alias py3="python3"
+
+
+# Fix typo I keep making
+alias :q="exit"
+alias emacs="emacsclient -c -a 'emacs'"
+alias u="uptime"
+
